ba31404dbc658ebbcba8dfa8b8c1e67b78143709
nithunkarur <rknithun@gmail.com>
2021-10-16 09:10:51 +0530

    Added: one day min amount and minimum amount to start every every

--- Gambler.java
+++ Gambler.java
@@ -1,13 +1,13 @@
 class Gambler{
-	//int everydayStake=100;
-	//int everyGameBet=1;
+	int everydayStake=100;
+	int everyGameBet=1;
 
 	public static void main(String[] args){
-		//Gambler g1=new Gambler();
+		Gambler g1=new Gambler();
 		System.out.println("WELCOME");
 		System.out.println("*******");
-		//System.out.println("The user can start for the day is:"+g1.everydayStake+"$");
-		//System.out.println("Minimum amount user can bet every game is:"+g1.everyGameBet+"$");
-		i
+		System.out.println("The user can start for the day is:"+g1.everydayStake+"$");
+		System.out.println("Minimum amount user can bet every game is:"+g1.everyGameBet+"$");
+		
 	}
 }
