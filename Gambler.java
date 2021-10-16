6f1827505386143f0502459ca761c158327d5e7e
nithunkarur <rknithun@gmail.com>
2021-10-16 09:15:45 +0530

    Added: win or loss

--- Gambler.java
+++ Gambler.java
@@ -1,6 +1,16 @@
+import java.util.Random;
 class Gambler{
 	int everydayStake=100;
 	int everyGameBet=1;
+	public void winLosse(){
+		Random r1=new Random();
+		int check=r1.nextInt(2);
+		if(check==0){
+			System.out.println("WIN");
+		}
+		else
+			System.out.println("LOSSE");
+	}
 
 	public static void main(String[] args){
 		Gambler g1=new Gambler();
@@ -8,6 +18,7 @@ class Gambler{
 		System.out.println("*******");
 		System.out.println("The user can start for the day is:"+g1.everydayStake+"$");
 		System.out.println("Minimum amount user can bet every game is:"+g1.everyGameBet+"$");
+		g1.winLosse();
 		
 	}
 }
