784ef6bae3eb2ef70ac12f6116fe66e67b69152d
nithunkarur <rknithun@gmail.com>
2021-10-17 17:11:35 +0530

    Calculated the bet for the monthly wise and displayed number of days won and lost with respective amount

--- Gambler.java
+++ Gambler.java
@@ -1,50 +1,69 @@
 import java.util.Random;
 class Gambler{
-	int tempEverydayStake=100;
+
 	int everyGameBet=1;
 	public void winLosse(){
 		int afterBet=0;
 		Random r1=new Random();
 
-		for (int i=1; i<=20; i++)
-		{	
-			tempEverydayStake=afterBet+100;
-			int stakeCheck=tempEverydayStake;
-			System.out.println("bet amount for day "+i+" is:"+tempEverydayStake);
+		for (int j=1; j<=12; j++)
+		{
+			int winCount=0;
+			int looseCount=0;
+			int profitAmt=0;
+			int looseAmt=0;
+			int tempEverydayStake=0;
+			System.out.println("**********");
+			System.out.println("MONTH "+j);
+			for (int i=1; i<=30; i++)
+			{	
+				tempEverydayStake=afterBet+100;
+				int stakeCheck=tempEverydayStake;
+				System.out.println("bet amount for day "+i+" is:"+tempEverydayStake);
 
-			int fPer=tempEverydayStake/2;
-        	        int winPer=tempEverydayStake+fPer;
-               		int loosePer=tempEverydayStake-fPer;
+				int fPer=tempEverydayStake/2;
+        	      	 	int winPer=tempEverydayStake+fPer;
+               			int loosePer=tempEverydayStake-fPer;
 
-			while(tempEverydayStake>loosePer && tempEverydayStake<winPer)
-			{
-				int winLoose=r1.nextInt(2);
-				if(winLoose==1)
+				while(tempEverydayStake>loosePer && tempEverydayStake<winPer)
 				{
-					//System.out.println("win");
-					afterBet=tempEverydayStake+everyGameBet;
+					int winLoose=r1.nextInt(2);
+					if(winLoose==1)
+					{
+						//System.out.println("win");
+						afterBet=tempEverydayStake+everyGameBet;
+					}
+					else
+					{
+						//System.out.println("loose");
+						afterBet=tempEverydayStake-everyGameBet;
+					}
+					tempEverydayStake=afterBet;
+					//System.out.println("after play="+afterBet);
+				}
+				System.out.println("-----------------------");
+				System.out.println("day "+i+" after the bet amount:"+tempEverydayStake);
+
+				if(stakeCheck<afterBet)
+				{
+					int profit=afterBet-stakeCheck;
+					winCount++;
+					profitAmt +=profit;
+					System.out.println("day "+i+" in profit:"+profit);
 				}
 				else
 				{
-					//System.out.println("loose");
-					afterBet=tempEverydayStake-everyGameBet;
+					int loose=stakeCheck-afterBet;
+					looseCount++;
+					looseAmt +=loose;
+					System.out.println("day "+i+" in loose:"+loose);
 				}
-				tempEverydayStake=afterBet;
-				//System.out.println("after play="+afterBet);
-			}
-			System.out.println("day "+i+" after the bet amount:"+tempEverydayStake);
-
-			if(stakeCheck<afterBet)
-			{
-				int profit=afterBet-stakeCheck;
-				System.out.println("day "+i+" in profit:"+profit);
-			}
-			else
-			{
-				int loose=stakeCheck-afterBet;
-				System.out.println("day "+i+" in loose:"+loose);
-			}
 			
+			}
+			System.out.println("WIN COUNT FOR MONTH "+j+" is::"+winCount);
+			System.out.println("MONTH "+j+" profit amt:"+profitAmt);
+			System.out.println("LOOSE COUNT FOR MONTH "+j+" is::"+looseCount);
+			System.out.println("MONTH "+j+" loost amt:"+looseAmt);
 		}
 
 	}
@@ -53,7 +72,7 @@ class Gambler{
 		Gambler g1=new Gambler();
 		System.out.println("WELCOME");
 		System.out.println("*******");
-		System.out.println("The user can start for the day is:"+g1.tempEverydayStake+"$");
+		System.out.println("The user can start for the day is:100$");
 		System.out.println("Minimum amount user can bet every game is:"+g1.everyGameBet+"$");
 		g1.winLosse();
 		
